73c99a8884a824b2645048d8167d480e69b7ef09
andrewxz <Andrew.Xiong@Teradata.com>
2016-04-14 10:51:39 -0400

    Revert "all changes" Revert a test This reverts commit 2bb45a041a960abc60fecd2b85eadec96f133788.

--- xx1.c
+++ xx1.c
@@ -7,11 +7,10 @@ int main() {
    int i = 0;
    int sum = 0;
    char *metoo = 0;
-   int a = 100;
 
    for (i = 0; i < MAX; i++) {
       sum += i;
-      metoo += sum + 100;
+      metoo += sum;
    }
 
    printf("Sum = %d, metoo = %d"; sum, metoo);
